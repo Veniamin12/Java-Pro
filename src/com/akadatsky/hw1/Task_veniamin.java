247fdee15c149bee241dcefc5629a040be234f8a
Nesteruk Venaimin <ven4ik11@gmail.com>
2023-09-10 16:22:03 +0300

    2. Homework2 method getDouble

--- src/com/akadatsky/hw1/Task_veniamin.java
+++ src/com/akadatsky/hw1/Task_veniamin.java
@@ -4,4 +4,9 @@ public class Task_veniamin {
     public  static  void  getMessage (int y){
         System.out.println("HEllo number" + y);
     }
+
+    public static  double getDouble (int u, int a){
+        double d = (double) (a+u)/100;
+        return d;
+    }
 }
